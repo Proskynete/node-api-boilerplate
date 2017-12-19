4aba1af22b9d73ac52d976866c4aa88f2e634906
Eduardo Alvarez Castañeda <eduardo.a1993@gmail.com>
2017-12-19 00:46:12 -0300

    Create connection with mongodb

--- serve.js
+++ serve.js
@@ -1,10 +1,25 @@
 'use strict';
 
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 
+// connect with DB
+const mongoose = require('mongoose');
+const nameDataBase = 'node-api-boilerplate';
+mongoose.Promise = global.Promise;
+mongoose.connect(`mongodb://localhost/${nameDataBase}`, {
+	useMongoClient: true
+}).then( () => console.log('DB Connected with Mongoose') )
+.catch( (err) => console.log(err));
+
+// Setting
 app.set('port', process.env.PORT || 3000);
 
+// Middlewers
+app.use(bodyParser.json());
+
+// init server
 app.listen(app.get('port'), () => {
 	console.log(`Server run on port ${app.get('port')}`);
 });
